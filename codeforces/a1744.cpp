0f82270d0dd74d602de0b428cfc7afbf538488a3
ahmed radwan <DevAhmedRadwan@gmail.com>
2022-10-18 14:35:26 +0200

    17/10/2022 codeforces problem solving session

---
+++ codeforces/a1744.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+void solve()
+{
+  int n;
+  std::cin >> n;
+  char dict[51] = {0};
+  int *int_letters = new int[n];
+  char *char_letters = new char[n + 1];
+  for (int i = 0; i < n; i++)
+  {
+    std::cin >> int_letters[i];
+  }
+  std::cin >> char_letters;
+  for (int i = 0; i < n; i++)
+  {
+    if (dict[int_letters[i]] == 0)
+      dict[int_letters[i]] = char_letters[i];
+    else if (dict[int_letters[i]] != char_letters[i])
+    {
+      std::cout << "NO" << std::endl;
+      return;
+    }
+  }
+  std::cout << "YES" << std::endl;
+  delete int_letters;
+  delete char_letters;
+  return;
+}
+
+int main()
+{
+  int t;
+  std::cin >> t;
+  for (int i = 0; i < t; i++)
+  {
+    solve();
+  }
+  return 0;
+}
