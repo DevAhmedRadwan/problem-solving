db2cd42950b7162f3e2ab29b5d7286aa69487224
ahmed radwan <DevAhmedRadwan@gmail.com>
2022-10-22 18:30:58 +0200

    22/10/2022 codeforces problem solving session

---
+++ codeforces/a337.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+#include <climits>
+
+int main()
+{
+  int n, m, max_diff = INT_MAX;
+  std::cin >> n >> m;
+  int *arr = new int[m];
+  for (int i = 0; i < m; i++)
+  {
+    std::cin >> arr[i];
+  }
+  std::sort(arr, arr + m);
+  for (int i = 0; i <= m - n; i++)
+  {
+    max_diff = std::min(max_diff, arr[i + n - 1] - arr[i]);
+  }
+  std::cout << max_diff;
+  return 0;
+}
